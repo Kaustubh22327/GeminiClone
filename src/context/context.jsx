22327c78cfd57e49c3ab8b17cec2042cc155053b
kaustubh0504 <kaustubh0504@gmail.com>
2024-04-14 00:07:03 +0530

    linked with API and project is showing result on taking inputs

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,9 +1,38 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import runChat from "../Config/gemini";
 export const Context = createContext();
 const ContextProvider = (props) => {
-  const contextValue = {};
+  const [input, setInput] = useState("");
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [showResult, setShowResult] = useState(false);
+  const [resultData, setResultData] = useState("");
+  const [loading, setLoading] = useState(false);
+  const onSent = async (prompt) => {
+    setResultData("");
+    setLoading(true);
+    const response = await runChat(input);
+    setResultData(response);
+    setLoading(false);
+    setShowResult(true);
+    setRecentPrompt(input);
+
+    setInput("");
+  };
+  const contextValue = {
+    prevPrompts,
+    setPrevPrompts,
+    onSent,
+    setRecentPrompt,
+    recentPrompt,
+    input,
+    setInput,
+    showResult,
+    resultData,
+    loading,
+  };
   return (
-    <Context.Provider value={contextvalue}>{props.children}</Context.Provider>
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
 };
 export default ContextProvider;
